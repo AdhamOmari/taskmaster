797886c6985dd792f04095703864efe6e9b53369
AdhamOmari <adhamomarics@gmail.com>
2021-11-01 13:56:43 +0200

    solve

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.taskmaster;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -26,5 +29,42 @@ public class MainActivity extends AppCompatActivity {
             Intent intent1 = new Intent(MainActivity.this, AllTasks.class);
             startActivity(intent1);
         });
+        Button sitting = findViewById(R.id.Settings);
+        sitting.setOnClickListener(view -> {
+            Intent intent1 = new Intent(MainActivity.this, Settingspage.class);
+            startActivity(intent1);
+        });
+        Button labButton = findViewById(R.id.Code);
+        labButton.setOnClickListener((view -> {
+            String taskTitle = labButton.getText().toString();
+            Intent goToTaskDetail = new Intent(MainActivity.this , Taskdetail.class);
+            goToTaskDetail.putExtra("taskName", taskTitle);
+            startActivity(goToTaskDetail);
+        }));
+        Button LAB = findViewById(R.id.LAB);
+        LAB.setOnClickListener((view -> {
+            String taskTitle = LAB.getText().toString();
+            Intent goToTaskDetail = new Intent(MainActivity.this , Taskdetail.class);
+            goToTaskDetail.putExtra("taskName", taskTitle);
+            startActivity(goToTaskDetail);
+        }));
+
+        Button prep = findViewById(R.id.prep);
+        prep.setOnClickListener((view -> {
+            String taskTitle = prep.getText().toString();
+            Intent goToTaskDetail = new Intent(MainActivity.this , Taskdetail.class);
+            goToTaskDetail.putExtra("taskName", taskTitle);
+            startActivity(goToTaskDetail);
+        }));
+
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String username = sharedPreferences.getString("username", "Your");
+        TextView userTasks = findViewById(R.id.textView8);
+        userTasks.setText(username);
+    }
+
 }
--- screenshots/README.md
+++ screenshots/README.md
@@ -5,3 +5,19 @@ and in each parent activity we have back arrow to the parent
 ![My task](screenshots/2021-10-31.png)
 ![image AllTasks](screenshots/2021-10-31 (2).png)
 ![image AddTasks](screenshots/2021-10-31 (1).png)
+
+
+
+# lab27
+
+## add the ability to send data among different activities in your application using SharedPreferences and Intents.
+
+# Create a Task Detail page
+# Create a Settings page.
+# add 3 button for tasks.
+
+![My task](lab272.PNG)
+![image AllTasks](lab27.PNG)
+![image AllTasks](lab273.PNG)
+
+![image AddTasks](screenshots/2021-10-31 (1).png)
