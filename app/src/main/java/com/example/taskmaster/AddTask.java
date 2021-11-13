738077346a022bbc8998ded556aad910f6ec4e32
AdhamOmari <adhamomarics@gmail.com>
2021-11-13 22:35:40 +0200

    solve

--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -1,11 +1,18 @@
 package com.example.taskmaster;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Task;
 
 public class AddTask extends AppCompatActivity {
 
@@ -39,15 +46,28 @@ public class AddTask extends AppCompatActivity {
 
                 EditText descriptionFiled = findViewById(R.id.descriptiondb);
                 String descr = descriptionFiled.getText().toString();
+                Toast.makeText(getApplicationContext(),"submitted!", Toast.LENGTH_SHORT).show();
 
-                Task task = new Task(bodyfiled, descr, titlefiled);
-                Long addedTaskID = AppDatabase.getInstance(getApplicationContext()).taskDao().insertTask(task);
 
-                System.out.println(
-                        "++++++++++++++++++++++++++++++++++++++++++++++++++" +
-                                " Student ID : " + addedTaskID
+                com.amplifyframework.datastore.generated.model.Task todo = Task.builder()
+                        .title(titlefiled)
+                        .body(bodyfiled)
+                        .state(descr)
+                        .build();
 
+                Amplify.API.mutate(
+                        ModelMutation.create(todo),
+                        response -> Log.i("AddTask", "Added Todo with id: " + response.getData().getId()),
+                        error -> Log.e("AddTask", "Create failed", error)
                 );
+//                Task task = new Task(bodyfiled, descr, titlefiled);
+//                Long addedTaskID = AppDatabase.getInstance(getApplicationContext()).taskDao().insertTask(task);
+//
+//                System.out.println(
+//                        "++++++++++++++++++++++++++++++++++++++++++++++++++" +
+//                                " Student ID : " + addedTaskID
+//
+//                );
 
 
             }
--- app/src/main/java/com/example/taskmaster/Settingspage.java
+++ app/src/main/java/com/example/taskmaster/Settingspage.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Settingspage extends AppCompatActivity {
 
